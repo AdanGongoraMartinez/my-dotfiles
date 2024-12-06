d99a75fd32f82a351f48327395bcf626ed00664d
adan-local <adangonmar@gmail.com>
2024-12-05 22:55:58 -0600

    Tokyio night config

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,17 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set -U fish_greeting "🐟"
-oh-my-posh init fish --config '~/.poshthemes/easy-term.omp.json' | source
+
+oh-my-posh init fish --config '~/.poshthemes/tokyonight_storm.omp.json' | source
+
 zoxide init fish | source
+
+# Set up fzf key bindings
+fzf --fish | source
+
+
+# git functions
+source ~/.config/fish/functions/git-functions.fish
+
+# fzf functions
+source ~/.config/fish/functions/fzf-functions.fish
